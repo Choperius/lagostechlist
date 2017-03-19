ab88bbf94013fa56304428e14533173a51365841
Alexander Holzmann Ekholm <alexander.holzmann.ekholm@gmail.com>
2017-03-19 02:31:00 +0100

    fifth commit

--- script.js
+++ script.js
@@ -1,21 +1,2 @@
 $(function() {
-	var myVideo =  iframe.getElementById('myVideo'); 
-	myVideo.mute();
-
-	$('.list').mouseover( function(e) {
-		$(".hover").removeClass("hover");
-		$(e.target).addClass("hover");
-		return false;
-	}).mouseout(function(e) {
-		$(this).removeClass("hover");
-	});
-	$('.img').hover( function() {
-		$(this).animate({
-			opacity: 50,
-			left: "+=50",
-			height: "toggle"
-		}, 5000, function() {
-				//Animation complete
-			})
-	})
 });
